d38c9230a853f4beeef2f0e4393702df2855c76b
Bill1907 <a31829660@gamil.com>
2024-07-20 11:54:44 +0900

    style init

---
+++ components/header.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+      <div className="">
+        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+          <Link href="/">Boseong Kim</Link>
+        </h1>
+        <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+          Frontend Engineer
+        </h2>
+      </div>
+      <nav className="nav hidden lg:block" aria-label="In-page jump links">
+        <ul className="flex flex-col gap-4">
+          <li>
+            <Link href="#about" className="text-lg font-medium text-slate-200">
+              <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                About
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#experience"
+              className="text-lg font-medium text-slate-200"
+            >
+              <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                Experience
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#projects"
+              className="text-lg font-medium text-slate-200"
+            >
+              <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                Project
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
