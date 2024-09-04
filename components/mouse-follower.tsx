bf769e545ab8c37de86053a2db172f50cb72bb84
boseong <boseongkim@autocrypt.io>
2024-09-04 17:35:04 +0900

    mouse cursor none

--- components/mouse-follower.tsx
+++ components/mouse-follower.tsx
@@ -19,14 +19,21 @@ export default function CenteredMouseFollower() {
   }, [handleMouseMove])
 
   return (
-    <div className="fixed inset-0 pointer-events-none z-50">
-      <div
-        className="absolute w-12 h-12 bg-primary opacity-50 rounded-full will-change-transform transition-transform duration-100 ease-out"
-        style={{
-          transform: `translate(${position.x}px, ${position.y}px) translate(-50%, -50%)`,
-        }}
-        aria-hidden="true"
-      />
-    </div>
+    <>
+      <div className="fixed inset-0 pointer-events-none z-50">
+        <div
+          className="absolute w-8 h-8 bg-primary opacity-50 rounded-full will-change-transform transition-transform duration-100 ease-out"
+          style={{
+            transform: `translate(${position.x}px, ${position.y}px) translate(-50%, -50%)`,
+          }}
+          aria-hidden="true"
+        />
+      </div>
+      <style jsx global>{`
+        body {
+          cursor: none;
+        }
+      `}</style>
+    </>
   )
 }
